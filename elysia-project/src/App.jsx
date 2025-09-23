2d9bfc702b6983e7fb396cfcdf8ffc90b07bba59
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2025-09-23 16:10:57 +0400

    assessment1

---
+++ elysia-project/src/App.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./App.css";
+//import react from "react";
+//import css from "./App.css";
+
+function App() {
+  const[glow, setGlow] = React.useState(""); // State to track which link is glowing
+  return (// code for the header section
+    <>
+    <header className="header">
+      {/* Logo + ribbon group */}
+      <div className="logo-box">
+        <img src="/elysia.png" alt="Elysia Logo" className="logo" />
+        <img src="/design (2).png" alt="Elysia " className="ribbon" />
+        <span className="name">Elysia</span> 
+      </div>
+
+      {/* Navigation */}
+      <nav>
+        <a
+          href="#Categories"
+          className={glow === "#Categories" ? "glow" : ""}// Apply glow class when the link is clicked
+          onClick={() => setGlow("#Categories")}
+        >
+          🐾 Categories
+        </a>
+        <a
+          href="#MyJourney"
+          className={glow === "#MyJourney" ? "glow" : ""} // Apply glow class when the link is clicked
+          onClick={() => setGlow("#MyJourney")}
+        >
+          🐾 My Journey
+        </a>
+        <a
+          href="#Sightings"
+          className={glow === "#Sightings" ? "glow" : ""} // Apply glow class when the link is clicked
+          onClick={() => setGlow("#Sightings")}
+        >
+          🐾 Sightings
+        </a>
+      </nav>
+
+      {/* Gold peel in top-right */}
+      <img src="/gold.png" alt="Gold Corner" className="gold" />
+    </header>
+
+    {/* Main content area */}
+  <main>
+  {/* Intro Section with Image + Text + Button */}
+  <section id="intro">
+    <img src="/mainbg.png" alt="Main Content" className="main_image" />
+    
+    <div className="intro-box">
+      <div className="introduction">
+       On the other side of the universe, the sounds of songs and roars.
+Freedom soars through the sky, where whispers of nature and an unknown 
+language are spoken by creatures — 
+  understood only by the creator.
+So, the creator formed a bridge between the human and natural worlds — 
+a place where they can meet and speak through emotion and action.
+      </div>
+      <button className="welcome">welcome to wildlife</button>
+    </div>
+  </section>
+
+  {/* ABOUT Label Section */}
+  <div className="label-container">
+    <img src="/label.png" alt="Label" className="label-img" />
+    <span className="label-text">ABOUT</span>
+    <img src="/bird.png" alt="Bird" className="bird" />
+    <img src="/about.png" alt="about" className="about" />
+  </div>
+
+</main>
+
+</>
+  );
+}
+
+export default App;
